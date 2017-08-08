b380dc677dee847f9b4cbb94569f67384939ca2c
newlogics <30690070+newlogics@users.noreply.github.com>
2017-08-08 20:48:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+  user: 'pk155mail',
+  database:'pk155mail',
+  host:'db.imad.hasura-app.io',
+  port:'5432',
+  password: process.env.DB_PASSWORD
+};
+
 
 var app = express();
 app.use(morgan('combined'));
+
+var pool = new Pool(config);
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -15,6 +27,21 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+
+app.get('/getuserlist', function (req, res) {
+    
+    pool.query('SELECT * FROM users', (err, result) => {
+      if(err)
+      {
+         res.status(500).send(err.toString()); 
+      }
+      else
+      {
+          res.send(JSON.stringify(result));
+      }
+    });
 });
 
 
