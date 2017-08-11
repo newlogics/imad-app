d7e71012b9ab4d1a481a40564d9ce604f9817063
newlogics <30690070+newlogics@users.noreply.github.com>
2017-08-11 23:17:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var http = require('http');
+var https = require('https');
 var Pool = require('pg').Pool;
 
 var config = {
@@ -53,15 +53,59 @@ app.get('/getuserlist', function (req, res) {
     });
 });
 
-function restart(dinosses,commitid,profile)
+function restart(dinoisses,commitid,profile)
 {
     var d = new Date();
     var n = d.toString(); 
     console.log(` ${n} restart ! ${dinosses}! ${commitid} ! ${profile}`);
+    
+    // create the JSON object
+    jsonObject = `{"gitRevision":"${commitid}","gitUrl":"https://github.com/${profile}/imad-app.git"}`;
+ 
+   // prepare the header
+   var postheaders = {
+    'Content-Type' : 'application/json',
+    'Content-Length' : Buffer.byteLength(jsonObject, 'utf8'),
+    'Cookie' : `dinoisses=${dinoisses}`
+   };
+ 
+    // the post options
+    var optionspost = {
+        host : 'imad-api.hasura.io',
+        port : 443,
+        path : '/restart',
+        method : 'POST',
+        headers : postheaders
+    };
+ 
+    console.log('Options prepared:');
+    console.log(optionspost);
+    console.log('Do the POST call');
+ 
+    // do the POST call
+    var reqPost = https.request(optionspost, function(res) {
+    console.log("statusCode: "+ res.statusCode);
+    // uncomment it for header details
+    console.log("headers: "+ res.headers);
+ 
+    res.on('data', function(d) {
+        console.log('POST result:' +d);
+        console.log('POST completed');
+        });
+    });
+ 
+    // write the json data
+    reqPost.write(jsonObject);
+    reqPost.end();
+    reqPost.on('error', function(e) {
+    console.log('error:' +e);
+    });
+ 
+ 
 }
 
 
-//setInterval(restart, 10000,"1","2","30");
+//setInterval(restart, 60000,"71t5upkmo96aibw8gdasauj74z8johao","85598ea9dc45d4019fa1cce3bbdd2199e2058170","newlogics");
 //900000
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
