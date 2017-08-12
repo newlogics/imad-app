e19eb5da8b06d4d79d041444477e388c3a12d64d
newlogics <30690070+newlogics@users.noreply.github.com>
2017-08-12 20:20:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,15 +65,10 @@ app.get('/restartserver', function (req, res) {
 });
 
 app.get('/24x7', function (req, res) {
-    if(req.cookies)
-    {
-        console.log(req.cookies);
-      res.sendFile(path.join(__dirname, 'ui', 'home.html'));
-    }
-    else
-    {
+        res.sendFile(path.join(__dirname, 'ui', 'home.html')); 
+});
+app.get('/login', function (req, res) {
         res.sendFile(path.join(__dirname, 'ui', 'login.html')); 
-    }
 });
 
 function restart(dinoisses,commitid,profile,uid)
