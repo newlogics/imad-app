ed1767aeb858b4a3360b732e7a478cce3ad5a21d
newlogics <30690070+newlogics@users.noreply.github.com>
2017-08-13 13:08:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.post('/validate', function (req, res) {
             console.log(req.body.zuid +" " + req.body.uemail);
             if(result.rows.length == 0)
               {
-                  pool.query('INSERT INTO restart (uid,email) VALUES ($1, $2)',[req.body.zuid,req.body.uemail ],(err, result) => {
+                  pool.query('INSERT INTO restart (uid,email) VALUES ($1, $2)',[req.body.zuid,req.body.uemail ],(err) => {
                     if(err)
                      {
                         res.status(500).send("failed "+ err); 
